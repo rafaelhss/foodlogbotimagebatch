c2a9b9fbdb0bb40d5b36126905a68e3b724f61e4
rafael <rafaelhss@gmail.com>
2017-09-09 18:19:06 -0300

    lets goo

--- index.js
+++ index.js
@@ -13,8 +13,8 @@ app.get('/bodypanel', function (req, res) {
           , height: 480
         },*/ 
         shotSize: {
-            /*width: 320
-          , */height: 'all'
+            width: 'all'
+          , height: 'all'
         }, /*
         userAgent: 'Mozilla/5.0 (iPhone; U; CPU iPhone OS 3_2 like Mac OS X; en-us)'
             + ' AppleWebKit/531.21.20 (KHTML, like Gecko) Mobile/7B298g',
