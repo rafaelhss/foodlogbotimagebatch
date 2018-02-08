8242e53734d6e7bde47f851fb8f2e449434258c6
Rafaelhss <rafaelhss@gmail.com>
2018-02-08 16:35:34 -0200

    Add files via upload

--- index.js
+++ index.js
@@ -114,7 +114,7 @@ function sendShot(url, fileName, options, usertelegram) {
 
 
 
-            var BOT_ID = "380968235:AAGqnrSERR8ABcw-_avcPN2ES3KH5SeZtNM";
+            var BOT_ID = "496888504:AAE3mU83hGmKfSYn0Z8vEhA-tDYM4lXOdI4";
             var chat_id = usertelegram;//"153350155";
             var UrlTemplate = "https://api.telegram.org/bot" + BOT_ID + "/sendPhoto?chat_id=" + chat_id;
 	    var UrlTextTemplate = "https://api.telegram.org/bot" + BOT_ID + "/sendmessage?chat_id="+ chat_id + "&text=" + encodeURIComponent(url);
